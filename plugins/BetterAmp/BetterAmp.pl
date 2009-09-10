5292dfd0db7ef1d976536137b8e66ad2d071a67e
Scott Boms <scott@wishingline.com>
2009-09-10 01:15:20 +0000

    Updated plugin code to work better with Movable Type 4.x and newer.

--- plugins/BetterAmp/BetterAmp.pl
+++ plugins/BetterAmp/BetterAmp.pl
@@ -7,18 +7,20 @@
 #
 # Requires Movable Type 4.x or newer
 #
-# Version 1.0
-# 2009/08/02
+# Version 1.0.1
+# 2009/09/09
 
 package MT::Plugin::BetterAmp;
 use strict;
 use warnings;
 
-use MT;
-use MT::Template::Context;
+require MT;
 use base qw( MT::Plugin );
+use MT::Util qw( rtrim );
+
+use MT::Template::Context;
 use vars qw($VERSION);
-our $VERSION = "1.0";
+our $VERSION = "1.0.1";
 
 my $plugin = MT::Plugin::BetterAmp->new({
   id          => 'betteramp',
@@ -29,21 +31,21 @@ my $plugin = MT::Plugin::BetterAmp->new({
   doc_link    => 'http://github.com/scottboms/mt-betteramp',
   icon        => 'ba-icon.gif',
   version     => $VERSION,
+  registry => {
+      tags => {
+        modifier => {
+          'better_amp' => \&better_amp,
+      },
+    },
+  },
 });
 
 MT->add_plugin($plugin);
 
-eval {require MT::Template::Context};
-unless ($@) {
-  require MT::Template::Context;
-  import MT::Template::Context;
-  MT::Template::Context->add_global_filter( better_amp => \&better_amp );
-}
-
 sub better_amp {
-    my $html = shift; # text to be parsed
-    $html =~ s/&(?!#?[xX]?(?:[0-9a-fA-F]+|\w{1,8});)/<span class='amp'>&amp;<\/span>/g;	
-    return $html;
+  my $html = shift; # text to be parsed
+  $html =~ s/&(?!#?[xX]?(?:[0-9a-fA-F]+|\w{1,8});)/<span class='amp'>&amp;<\/span>/g;
+  return $html;
 }
 
 1;
